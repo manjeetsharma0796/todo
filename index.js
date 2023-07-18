3e746b74f83c489c6f9c93df3d7aa148a7391e7d
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-18 15:43:42 +0530

    Add appendTodo, which appends todo into container

--- index.js
+++ index.js
@@ -6,8 +6,33 @@ const removeAllChild = (element) => {
   }
 };
 
-window.onload = () => {
+const createTodoElement = (todoText) => {
+  const todoNode = document.createElement('p');
+  todoNode.innerText = todoText;
+  return todoNode;
+};
+
+const appendTodo = (container, todoElements) => {
+  todoElements.forEach(todoElement => {
+    container.appendChild(todoElement);
+  });
+};
+
+const main = () => {
   const todoListContainer = getTodoContainer();
-  console.log(todoListContainer);
+  const todoList = ['Buy eggs from supermarket.',
+    'Finish Code Of Conduct training on campus.',
+    'Finish day 15 part 2 of Advent of Code problem.'];
+
+
   removeAllChild(todoListContainer);
-}
+  const todoElements = todoList.map((todo) => {
+    return createTodoElement(todo);
+  })
+
+  appendTodo(todoListContainer, todoElements);
+};
+
+window.onload = () => {
+  main();
+};
