48f34412b2be46d9ba8c7dc88b6a38dfcaee7152
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-19 17:53:16 +0530

    Remove: appendTask, markTask, clearMarkFromTask

--- todo-task.js
+++ todo-task.js
@@ -4,20 +4,9 @@ const createTaskElement = (name) => {
   return taskElement;
 };
 
-const appendTask = (container, taskElements) => {
-  taskElements.forEach(taskElement => {
-    container.appendChild(taskElement);
-  });
-};
-
-
-
-const markTask = (taskElement) => taskElement.classList.add("marked");
-const clearMarkFromTask = (taskElement) => taskElement.classList.remove("marked");
-
 const addClickEvent = (taskElement) => {
   taskElement.onclick = () => {
-    markTask(taskElement);
+    taskElement.classList.add("marked");
 
     taskElement.onclick = () => {
       taskElement.classList.remove("marked");
@@ -29,19 +18,19 @@ const addClickEvent = (taskElement) => {
 const main = () => {
   const addTask = document.querySelector("#add-task");
   const todoListContainer = document.querySelector("#todo-list");
-  const taskDetailsElement = document.querySelector("#task-details");
+  const taskDetails = document.querySelector("#task-details");
   const taskList = [];
 
-  const onNewTask = () => {
-    const task = taskDetailsElement.value;
+  const createTask = () => {
+    const task = taskDetails.value;
     taskList.push({ task, taskCompleted: false });
-    taskDetailsElement.value = "";
+    taskDetails.value = "";
     const taskElement = createTaskElement(task);
     addClickEvent(taskElement);
     todoListContainer.append(taskElement);
   };
 
-  addTask.onclick = onNewTask;
+  addTask.onclick = createTask;
 };
 
 window.onload = main;
